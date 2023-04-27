1db464df0173308d777267de837cbeba8715f217
Nehemias Ulloa <nulloa1@iastate.edu>
2023-04-26 20:46:12 -0700

    Add initial waitTimes function R file

---
+++ R/waitTimes.R
@@ -0,0 +1,8 @@
+#' Get wait times
+#'
+#' @param park The park name
+#' @param dates A two element character vector date range
+#' @return A dataframe
+#'
+#' @examples
+#' waitTimes("Universal", "01-02-2023")
