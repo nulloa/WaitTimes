5448a99c706b1835c8480079ae41ecdeb9c274e4
Nehemias Ulloa <nulloa1@iastate.edu>
2023-04-30 22:01:07 -0700

    Change park argument to park id argument

--- R/waitTimes.R
+++ R/waitTimes.R
@@ -1,8 +1,30 @@
 #' Get wait times
+#' 
+#' This function will pull wait times for all rides from a given park. The data
+#' comes from . See all the available theme parks using the 
+#' \code{\link{parks}} function.
+#' 
 #'
-#' @param park The park name
+#' @param parkid The park id for the park you'd like to see
 #' @param dates A two element character vector date range
 #' @return A dataframe
 #'
 #' @examples
-#' waitTimes("Universal", "01-02-2023")
+#' waitTimes("66", "01-02-2023")
+#' 
+#' @export
+waitTimes <- function(parkid, dates){
+  
+  tmp <- jsonlite::fromJSON(txt = paste0("https://queue-times.com/en-US/parks/", parkid, "/queue_times.json"))
+  
+  
+  
+  df <- do.call(rbind, lapply(seq_along(tmp$lands), function(x) {
+    tmp$lands[[x]]$land <- names(tmp$lands)[x]
+    tmp$lands[[x]]
+  }))
+  
+}
+
+
+
