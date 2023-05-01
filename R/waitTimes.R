56a0b98a523afaf23532f74aa7a20b1f1cc2bdd6
Nehemias Ulloa <nulloa1@iastate.edu>
2023-04-30 22:13:38 -0700

    Fix roxygen and add working function

--- R/waitTimes.R
+++ R/waitTimes.R
@@ -1,8 +1,8 @@
 #' Get wait times
 #' 
 #' This function will pull wait times for all rides from a given park. The data
-#' comes from . See all the available theme parks using the 
-#' \code{\link{parks}} function.
+#' comes from . See all the available theme parks using the \code{data(parks)}.
+#' Will need internet to run.
 #' 
 #'
 #' @param parkid The park id for the park you'd like to see
@@ -17,12 +17,24 @@ waitTimes <- function(parkid, dates){
   
   tmp <- jsonlite::fromJSON(txt = paste0("https://queue-times.com/en-US/parks/", parkid, "/queue_times.json"))
   
+  if(length(tmp$lands!=0)){
+    # park has lands
+    
+    # Set lands to names of lists
+    names(tmp$lands$rides) <- tmp$lands[,2]
+    
+    # merge lands while adding land names
+    df <- do.call(rbind, lapply(seq_along(tmp$lands$rides), function(x) {
+      tmp$lands$rides[[x]]$land <- names(tmp$lands$rides)[x]
+      tmp$lands$rides[[x]]
+    }))
+    
+  }else{
+    # park has no lands
+    df <- cbind(tmp$rides, land = NA)
+  }
   
-  
-  df <- do.call(rbind, lapply(seq_along(tmp$lands), function(x) {
-    tmp$lands[[x]]$land <- names(tmp$lands)[x]
-    tmp$lands[[x]]
-  }))
+  return(df)
   
 }
 
