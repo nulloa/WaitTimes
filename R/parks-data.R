239abddf92ddd574ec9c689376842748589a1d95
Nehemias Ulloa <nulloa1@iastate.edu>
2023-04-30 22:00:44 -0700

    Add description file for parks dataset

---
+++ R/parks-data.R
@@ -0,0 +1,18 @@
+#' Theme park data on WaitTimes
+#'
+#' Data from \href{queue-times.com/en-US}{Queue-Times.com} from all the theme
+#' parks available. Data includes name and location data.
+#'
+#' @docType data
+#'
+#' @usage data(parks)
+#'
+#' @format An object of class \code{"data.frame" }
+#'
+#' @keywords datasets
+#'
+#' @source \href{queue-times.com/en-US}{Queue-Times.com}
+#'
+#' @examples
+#' data(parks)
+"parks"
